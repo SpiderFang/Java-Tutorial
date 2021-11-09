fbdc91ff86fea85d64b4cc19d16145751c93b4b0
Wei-Feng Fang <wffang@gmail.com>
2021-11-09 17:39:30 +0800

    Create Counting-4.java

---
+++ Counting-4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+    System.out.println ("+1000");
+    
+    int total = 1000;
+    int cost = 0;
+    for (int i = 0; i < 5; i++) {
+	    int a = sc.nextInt ();
+	    System.out.printf("-%04d\n", a);
+	    cost -= a; //計算總支出
+    }
+    
+    System.out.println ("-----");
+    
+    if (total > 0)
+      {
+	System.out.printf("+%04d", total);
+      }
+    else
+      {
+	total *= -1;
+	System.out.printf("-%04d", total);
+      }
+
+  }
+}
